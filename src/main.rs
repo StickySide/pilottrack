ddeed0d8ff222905eac29b2790817966387355d8
Jon <jnorton6@gmail.com>
2026-08-21 11:50:23 -0600

    Use reqwest to get ics

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 use anyhow::Result;
+use reqwest::header::USER_AGENT;
 use std::env;
+
 fn main() -> Result<()> {
     dotenvy::dotenv().ok();
 
     let username = env::var("USERNAME")?;
     let password = env::var("PASSWORD")?;
+    let url = env::var("URL")?;
 
-    println!("Username: {}", username);
-    println!("Password: {}", password);
+    let client = reqwest::blocking::Client::builder().build()?;
+
+    println!("U: {}\nP: {}\nURL: {}", username, password, url);
+
+    let result = client
+        .get(&url)
+        .basic_auth(username, Some(password))
+        .header(USER_AGENT, "curl/8.5.0")
+        .send()?;
+
     Ok(())
 }
