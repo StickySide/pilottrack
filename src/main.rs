fa22cddcaf175c22825519ec477efe51a60c8256
Jon <jnorton6@gmail.com>
2026-08-21 21:51:03 -0600

    Add parsing for fight number and struct for storing leg data

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 use anyhow::{Context, Result};
+use chrono::NaiveDateTime;
 use icalendar::{Calendar, CalendarDateTime, Component, DatePerhapsTime, Event};
 use reqwest::header::USER_AGENT;
 use std::env;
+
+#[allow(dead_code)]
+struct Leg {
+    flight_number: String,
+    dep_time: NaiveDateTime,
+    arr_time: NaiveDateTime,
+    dep_icao: String,
+    arr_icao: String,
+}
 
 fn main() -> Result<()> {
     // Read env from file
@@ -17,13 +27,23 @@ fn main() -> Result<()> {
     // Parse calendar
     let calendar: Calendar = calendar.parse().unwrap();
 
-    // Prints summary for next event
-    println!(
-        "{}",
-        get_next_event(&calendar).unwrap().get_summary().unwrap()
-    );
+    if let Some(event) = get_next_event(&calendar) {
+        println!("{}", get_flight_number(&event)?)
+    }
+    // let next_leg = Leg {
+    // };
 
     Ok(())
+}
+
+fn get_flight_number(event: &Event) -> Result<String> {
+    let flight_number = String::from("UAL")
+        + String::from(event.get_summary().unwrap())
+            .split(' ')
+            .next()
+            .unwrap();
+
+    Ok(flight_number)
 }
 
 fn get_next_event(cal: &Calendar) -> Option<Event> {
